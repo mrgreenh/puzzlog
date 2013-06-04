88a56d7ffdbbee95601441dd301dc43223d39967
Carlo Andrea Conte <carloandreaconte@icloud.com>
2013-06-04 12:57:31 +0200

    Adjusted layout in box and fragment images tab. Adjusted bug preventing images to be added from the box with multiple openings of the box.

--- app/assets/javascripts/fragment_image_relationships.js
+++ app/assets/javascripts/fragment_image_relationships.js
@@ -1,9 +1,10 @@
 function addImagesFromBoxConfirmationButton(){
 	$("#addItemsFromBoxConfirmationButton").click(function(){
-		var selectedImages = [];
+		var selectedImages = {};
 		$("#box_resources_list_container .multiple_selection_checkbox").each(function(){
 			if(this.checked){
-				selectedImages.push(JSON.parse($(this).parents("li").attr("data-image-json")));
+				var imageData = JSON.parse($(this).parents("li").attr("data-image-json"));
+				selectedImages[imageData.id]=imageData;
 			}
 		});
 		fragmentImagesInjection(selectedImages);
