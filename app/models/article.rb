4be547fd0708cfeab429b72cc00fd3d668bc89a3
mrgreenh <carloandreaconte@icloud.com>
2013-10-03 17:50:55 -0400

    Added articles deletion dependency, added link to menu entry title

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,8 @@ class Article < ActiveRecord::Base
   has_many :pages, dependent: :destroy
   
   has_many :fragments, through: :pages, source: :fragments
+
+  has_many :menu_entries, dependent: :destroy
   
   belongs_to :user
   
