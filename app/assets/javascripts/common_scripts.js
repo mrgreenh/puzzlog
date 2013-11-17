5c4a6cec77a0b35575c26adce76e94f1aacf45ff
mrgreenh <carloandreaconte@icloud.com>
2013-11-17 16:27:00 -0500

    Final fixes for using puzzlog as a blog...

--- app/assets/javascripts/common_scripts.js
+++ app/assets/javascripts/common_scripts.js
@@ -1,45 +1,45 @@
 //Stateful loading buttons: classi .stateful_loading e attributo data-loading-text
 function stateful_loading(element){
-	var bottone = element;
-	var text = bottone.attr("data-loading-text");
-	bottone.val(text);
-	bottone.html(text);
-	bottone.attr("disabled","disabled");
+        var bottone = element;
+        var text = bottone.attr("data-loading-text");
+        bottone.val(text);
+        bottone.html(text);
+        bottone.attr("disabled","disabled");
 }
 
 //Flash messages slide up after some seconds
 function flashMessages(){
-	timeoutID = window.setTimeout(function(){
-		$("#flash_messages_container").slideUp();
-	}, 5000);
+        timeoutID = window.setTimeout(function(){
+                $("#flash_messages_container").slideUp();
+        }, 5000);
 }
 
 //Funzione che mostra il contenuto passatogli in una finestra modale
 function modalWindow(title,content){
-	$("#genericModalWindow h3").html(title);
-	$("#genericModalWindow .modal-body").html(content);
-	$("#genericModalWindow").modal('show');
-	$("[data-original-title]").popover('hide');
+        $("#genericModalWindow h3").html(title);
+        $("#genericModalWindow .modal-body").html(content);
+        $("#genericModalWindow").modal('show');
+        $("[data-original-title]").popover('hide');
 }
 function closeModal(){
-	$("#genericModalWindow").modal('hide');
+        $("#genericModalWindow").modal('hide');
 }
 
 //Popover with partial's controls
 function initializeControlsPopovers(){
-	$(".fragment_container .controls_popover_toggler,.fragment_summary .controls_popover_toggler,.resource_partial .controls_popover_toggler").click(function(){
-    	//Hiding some buttons in specific situations
-    	$(".article_edit_fragments_container .hidden_on_article_editing").css("display","none");
-    	
-    	var container = $(this).parents(".resource_partial");
-      	var overlay_content = container.find(".tooltip_content").html();
-    	container.prepend("<div class='popover_overlay'><div class='overlay_content'>"+overlay_content+"</div></div>");
-    	var overlay = container.children(".popover_overlay");
-    	overlay.fadeIn();
-    	overlay.find(".close, .btn").click(function(){
-    		overlay.fadeOut(function(){overlay.remove();});
-    	});
-    	
+        $(".fragment_container .controls_popover_toggler,.fragment_summary .controls_popover_toggler,.resource_partial .controls_popover_toggler").click(function(){
+        //Hiding some buttons in specific situations
+        $(".article_edit_fragments_container .hidden_on_article_editing").css("display","none");
+
+        var container = $(this).parents(".resource_partial");
+        var overlay_content = container.find(".tooltip_content").html();
+        container.prepend("<div class='popover_overlay'><div class='overlay_content'>"+overlay_content+"</div></div>");
+        var overlay = container.children(".popover_overlay");
+        overlay.fadeIn();
+        overlay.find(".close, .btn").click(function(){
+                overlay.fadeOut(function(){overlay.remove();});
+        });
+
     });
 }
 
@@ -47,10 +47,15 @@ function initializeControlsPopovers(){
 $(".fragment_summary td.table-value p,.fragment_summary td.table-key p,.fragment_summary ul.summary_hash_container, .thumbnail p").dotdotdot();
 
 $(function(){
-	initializeControlsPopovers();
-	flashMessages();
-	$("li a.create_fragment_button").tooltip({placement:'bottom'});
-        $(".load_streamline_button").click(function(){
-	    stateful_loading($(this));
-	});
+    initializeControlsPopovers();
+    flashMessages();
+    $("li a.create_fragment_button").tooltip({placement:'bottom'});
+
+    //Other buttons generally available in the website
+    $("#load_streamline_button").click(function(){
+        stateful_loading($(this));
+    });
+    $("#mobile_title").click(function(){
+        $(".customized_menu_entries").toggleClass("hide");
+    });
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def is_blog_initialized?
     User.count != 0
   end
-  
+
   def full_title(custom_title)
     base_title = "PuzzLog"
     if custom_title.empty?
@@ -12,28 +12,28 @@ module ApplicationHelper
       "#{custom_title} | #{base_title}"
     end
   end
-  
+
   def base_url
     "puzzlog.herokuapp.com"
   end
-  
+
   def app_domain
     "herokuapp.com"
   end
-  
+
   def twitter_id
     "69243134"
   end
-  
+
   def build_streamline(index=0,count=8,user=nil)
      @articles = ArticlesHelper.streamline(index,(count/2).to_i,user)
      streamline_fragments = fragments_streamline(index,(count/2).to_i,user)
      @streamline_elements = (@articles+streamline_fragments).sort_by(&:publication_date).reverse
-    
+
      #per caricare gli script e gli stili
      @fragments = article_summaries_fragments(@articles)+streamline_fragments
      @fragment_types = getFragmentTypes(@fragments)
-    
+
      @index = index.to_i + 1
      @count = count
   end
@@ -59,7 +59,7 @@ module ApplicationHelper
   def has_role?(name, user=current_user)
     !user.nil? && user.roles.find_all_by_name(name).any?
   end
-  
+
   def can_modify_blog?
     current_user && is_superadmin?
   end
